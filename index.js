108855db86576cb39b87b347ef42123782f5a5b6
Omomayowa Valda I <112013755+Valda7@users.noreply.github.com>
2023-02-28 01:11:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function multiply(5,10){
+    return 5*10;
+}
+console.log(multiply(5,10)); // output : 50
+
+//creating variables
+
+var firstName = "Omomayowa";
+var lastName = "Idowu";
+var stuternTrack = "Frontend"
+var skills = ["Writing","HTML","CSS", "Javascript"]
+var favoriteCcolour = "Blue"
+var yearOfCountryIndependence = "1960"
+
+
+"My name is Omomayowa Idowu and I'm currently studying the Frontend track at Stutern. I have skills in Writing, HTML, CSS and Javascript. My favorite color is blue and my country got it independence in the year 1960"
